e462c97f987c778277e63152231fb676210835d7
bchou0 <bchou515@gmail.com>
2017-04-10 17:31:35 -0400

    display method added and board

--- hello-world.rb
+++ hello-world.rb
@@ -1,3 +1,29 @@
-puts "hello world!"
+#connect 4 is a 7x6 board
+# O O O O O O O
+# O O O O O O O
+# O O O O O O O
+# O O O O O O O
+# O O O O O O O
+# O O O O O O O
 
-print "Kenny did you get this? It's me Brian"
+board = [
+  [" "," "," "," "," "," "],
+  [" "," "," "," "," "," "],
+  [" "," "," "," "," "," "],
+  [" "," "," "," "," "," "],
+  [" "," "," "," "," "," "],
+  [" "," "," "," "," "," "]
+]
+
+def display(board)
+display_string = ""
+
+  board.each do |row|
+    row.each do |cell|
+      display_string << "#{cell} "
+    end
+    display_string << "\n"
+  end
+print display_string
+
+end
